85b823861ae276743a2d0c583933b81fe5087da5
rmss828 <ramezazimi17@gmail.com>
2023-06-12 00:23:26 +0200

    used prettier

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-import Navbar from './Navbar';
-import Hero from './Hero';
-import RSSFeed from './RSSFeed';
-import Signature from './Signature';
-import Footer from './Footer';
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import RSSFeed from "./RSSFeed";
+import Signature from "./Signature";
+import Footer from "./Footer";
 
 export { Footer, Hero, Navbar, RSSFeed, Signature };
