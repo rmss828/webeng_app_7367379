5716c7f732d923bc12fb13597844e9d98a8814e9
rmss828 <ramezazimi17@gmail.com>
2023-06-12 23:02:35 +0200

    added meta data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 /*
   main component of website
   renders overall layout of website
+  i sadly didn't have a nice image for the meta data so you'll get the one vite provides
 */
 
 import styles from "./style";
 import { Navbar, Hero, RSSFeed, Signature, Footer } from "./components";
+import { Helmet } from "react-helmet";
 
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
+      <Helmet>
+        <title>7367379 - WebEngProject</title>
+        <meta name="description" content="This is my web engineering project." />
+      </Helmet>
+
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
